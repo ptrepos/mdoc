c2e899b6061b8f808991e01c5463d4268809ab5f
takuo hada <t_hada@dachicraft.net>
2016-09-09 22:53:42 +0900

    コマンドラインツール開発

--- src/MdocConv/Program.cs
+++ src/MdocConv/Program.cs
@@ -10,32 +10,137 @@ namespace MdocConv
 {
     class Program
     {
+        public const string HTML_TYPE = "html";
+
         static void Main(string[] args)
         {
-            string mdocPath = args[0];
-            string outputPath = Path.Combine(
-                    Path.GetDirectoryName(mdocPath),
-                    Path.GetFileNameWithoutExtension(mdocPath) + ".html");
+            if (ParseArgs(args) == false)
+                return;
+
+            string mdocPath = inputFile;
+            string outputPath = Path.Combine(outputDir, outputFile);
 
             Section[] sections = Mdoc.Mdoc.Parse(mdocPath);
 
-            using (StreamWriter writer = new StreamWriter(outputPath))
+            if (type == HTML_TYPE)
             {
-                writer.WriteLine("<!DOCTYPE html>");
-                writer.WriteLine("<html>");
-                writer.WriteLine("<head>");
-                writer.WriteLine("<meta charset=\"UTF-8\">");
-                writer.WriteLine("<title>{0}</title>", Path.GetFileNameWithoutExtension(mdocPath));
-                writer.WriteLine("<link href=\"style/doc.css\" rel=\"stylesheet\" type =\"text/css\" />");
-                writer.WriteLine("</head>");
-                writer.WriteLine("<body>");
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    writer.WriteLine("<!DOCTYPE html>");
+                    writer.WriteLine("<html>");
+                    writer.WriteLine("<head>");
+                    writer.WriteLine("<meta charset=\"UTF-8\">");
+                    writer.WriteLine("<title>{0}</title>", Path.GetFileNameWithoutExtension(mdocPath));
+                    writer.WriteLine("<link href=\"{0}\" rel=\"stylesheet\" type =\"text/css\" />", cssFile);
+                    writer.WriteLine("</head>");
+                    writer.WriteLine("<body>");
 
-                HtmlEncoder encoder = new HtmlEncoder();
-                encoder.Encode(writer, sections);
+                    HtmlEncoder encoder = new HtmlEncoder();
+                    encoder.Encode(writer, sections);
 
-                writer.WriteLine("</body>");
-                writer.WriteLine("</html>");
+                    writer.WriteLine("</body>");
+                    writer.WriteLine("</html>");
+                }
             }
+        }
+
+        private static string cssFile = null;
+        private static string outputDir = null;
+        private static string outputFile = null;
+        private static string inputFile = null;
+        private static string type = null;
+
+        private static bool ParseArgs(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith("--css="))
+                {
+                    cssFile = arg.Substring("--css=".Length);
+                }
+                else if (arg.StartsWith("--output-dir="))
+                {
+                    outputDir = arg.Substring("--output-dir=".Length);
+                }
+                else if (arg.StartsWith("--type="))
+                {
+                    type = arg.Substring("--type=".Length);
+                }
+                else if (arg.StartsWith("--output-file="))
+                {
+                    outputFile = arg.Substring("--output-file=".Length);
+                }
+                else
+                {
+                    inputFile = arg;
+                }
+            }
+
+            if (string.IsNullOrEmpty(inputFile))
+            {
+                Console.Error.WriteLine("input file is nothing.");
+
+                ShowCommandline();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(outputDir))
+            {
+                outputDir = Path.GetDirectoryName(inputFile);
+            }
+
+            if (string.IsNullOrEmpty(cssFile))
+            {
+                cssFile = "style/doc.css";
+            }
+
+            if (string.IsNullOrEmpty(type))
+            {
+                type = HTML_TYPE;
+            }
+
+            if (string.IsNullOrEmpty(outputFile))
+            {
+                outputFile = Path.GetFileNameWithoutExtension(inputFile) + "." + type;
+            }
+
+            return true;
+        }
+        private static void ShowCommandline()
+        {
+            Console.WriteLine(@"> MdocConv [options] inputFile
+## Options
+
+--output-dir='Output Directory'
+        Output directory. File name is auto generate.
+
+--output-file=='Output File'
+        Output file name.
+
+--css='CSS File Name'
+        HTML stylesheet file name.
+
+--type='output type'
+        Supported format ""html"".
+");
+
+
+            //if (arg.StartsWith("--css="))
+            //{
+            //    ccsFile = arg.Substring("--ccs=".Length);
+            //}
+            //else if (arg.StartsWith("--output-dir="))
+            //{
+            //    outputDir = arg.Substring("--output-dir=".Length);
+            //}
+            //else if (arg.StartsWith("--type="))
+            //{
+            //    type = arg.Substring("--type=".Length);
+            //}
+            //else if (arg.StartsWith("--output-file="))
+            //{
+            //    outputFile = arg.Substring("--output-file=".Length);
+            //}
         }
     }
 }
