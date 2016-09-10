55729f38605845be3f8ee5d4111a53f52cdc6ab8
takuo hada <t_hada@dachicraft.net>
2016-09-10 22:31:47 +0900

    エラーメッセージと行番号が表示できるよう修正中

---
+++ src/Mdoc/Parsers/MdocParseException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mdoc.Parsers
+{
+    public class MdocParseException : Exception
+    {
+        private int line;
+
+        public MdocParseException(string message, int line) : base(string.Format("[line:{0}] ", line) + message)
+        {
+            this.line = line;
+        }
+
+        public int Line
+        {
+            get { return line; }
+        }
+    }
+}
